2a0753ce8214764e6f6b290893ec2324d7d46b59
Noelle Daley <adriannenoelle@gmail.com>
2015-07-01 21:26:25 -0700

    First function working

--- melon-sales-report/accounting.py
+++ melon-sales-report/accounting.py
@@ -1,26 +1,52 @@
+"""
+* Counts the amounts of each type of melon that were sold
+* Calculates the revenue from those melon tallies
+* Separates sales into online sales and phone sales
+* Produces fancy report to summarize the info for our CEO
+
+Ordering tasks into their own functions would be a great
+start!
+"""
+
 SALESPERSON_INDEX = 0
 INTERNET_INDEX = 1
-DORKY_LINE_LENGTH = 80
 
-print "*" * DORKY_LINE_LENGTH
-f = open("orders-by-type.txt")
-melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+def print_divider():
+    print '*' 80
 
-for l in f:
-    data = l.split("|")
-    melon_type = data[1]
-    melon_count = int(data[2])
-    melon_tallies[melon_type] += melon_count
+def count_melons(orders_file):
+    """Takes a file and returns the amount of each type of melon that was sold.
+    """
 
-f.close()
-melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
-total_revenue = 0
-for melon_type in melon_tallies:
-    price = melon_prices[melon_type]
-    revenue = price * melon_tallies[melon_type]
-    total_revenue += revenue
-    print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+    fin = open(orders_file)
+    melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+
+    print "*" * 80
+
+    for line in fin:  # switched variable name from l to line to be more clear
+        data = line.split("|")
+        melon_type = data[1]
+        melon_count = int(data[2])
+        melon_tallies[melon_type] += melon_count
+
+print_divider()
+count_melons("orders-by-type.txt")
+
+def calculate_revenue(melon_tallies):
+    """Takes a dictionary of melon tallies and returns the sales revenue.
+    """
+
+    melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
+    total_revenue = 0
+    
+    for melon_type in melon_tallies:
+        price = melon_prices[melon_type]
+        revenue = price * melon_tallies[melon_type]
+        total_revenue += revenue
+        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+
 print "******************************************"
+
 f = open("orders-with-sales.txt")
 sales = [0, 0]
 for line in f:

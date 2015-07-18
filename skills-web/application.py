ceb7506d3dc22a03ad370a42d423654dd3613c33
Noelle Daley <adriannenoelle@gmail.com>
2015-07-18 10:18:49 -0700

    adding first route

--- skills-web/application.py
+++ skills-web/application.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def start_application():
+    return render_template('application.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

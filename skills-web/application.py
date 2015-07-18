f5f067c1fc2bca8e9746455d4ba21a141781639b
Noelle Daley <adriannenoelle@gmail.com>
2015-07-18 10:23:24 -0700

    adding templates folder

--- skills-web/application.py
+++ skills-web/application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start_application():
-    return render_template('application.html')
+    return render_template('application-form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

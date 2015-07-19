e32c1d586d61638490ff12a85d4ba384b9f3768d
Noelle Daley <adriannenoelle@gmail.com>
2015-07-19 12:38:34 -0700

    pulled variables from form into confirmation

--- skills-web/application.py
+++ skills-web/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,11 @@ def start_application():
 
 @app.route('/application', methods=["POST"])
 def confirm_application():
-    return render_template('confirmation.html')
+    first_nm = request.form.get("first_name")
+    salary = request.form.get("salary_requirement")
+    job_app = request.form.get("job")
+
+    return render_template('confirmation.html', first_name = first_nm, salary_requirement = salary, job = job_app)
 
 if __name__ == '__main__':
     app.run(debug=True)

b3b7c1cf96a1f16daa9391cecee3c12551a331d3
Noelle Daley <adriannenoelle@gmail.com>
2015-07-22 21:55:38 -0700

    pulled variables from html form

--- ajax-prep/profile.py
+++ ajax-prep/profile.py
@@ -16,12 +16,20 @@ def index():
 def profile():
     """Return results from profile form."""
 
-    fullname = request.form['name']
+    user_name = request.form['name']
+    user_age = request.form.get('age')
+    user_occupation = request.form.get('occupation')
+    user_salary = request.form.get('salary')
+    user_edu = request.form.get('education')
+    user_state = request.form.get('state')
+    user_city = request.form.getlist('city')
+    user_garden = request.form.get('garden')
+    user_tv = request.form.get('tv')
 
     # TODO: get the values from the rest of the form
     # Add them to jsonify
 
-    return jsonify({'fullname': fullname, })
+    return jsonify({'user_name': user_name, })
 
 
 
--- ajax-prep/static/profile.js
+++ ajax-prep/static/profile.js
@@ -1,11 +1,11 @@
 
 function submitProfile(evt) {
     evt.preventDefault();
-    
+
     $.post("/profile",
         $('#profile-form').serialize(),
         function (result) {
-            $('#profile').append("<p>" + "Name: " + result.fullname + "</p>");
+            $('#profile').append("<p>" + "Name: " + result.user_name + "</p>");
         }
     );
 }

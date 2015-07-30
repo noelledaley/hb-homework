8c6ff33e885f3e123353c780ac891f8fb48b97b6
Noelle Daley <adriannenoelle@gmail.com>
2015-07-29 19:24:23 -0700

    doctests done

--- doctests/helpers.py
+++ doctests/helpers.py
@@ -1,6 +1,6 @@
-#Please add some doctests to the following file! Otherwise, how will we ever
-#test the results of these super complicated, higher level functions???!!!
-#Try to create a few doctests for each that take in interesting data. 
+# Please add some doctests to the following file! Otherwise, how will we ever
+# test the results of these super complicated, higher level functions???!!!
+# Try to create a few doctests for each that take in interesting data.
 
 from doctest import testmod
 
@@ -8,6 +8,11 @@ from doctest import testmod
 def all_even(number_list):
     """Return a list of only the even numbers in the input list.
 
+    >>> all_even([0, 1, 2, 3, 4])
+    [0, 2, 4]
+
+    >>> all_even([-7, 0, -2, 1, 2])
+    [0, -2, 2]
 
     """
 
@@ -17,6 +22,14 @@ def all_even(number_list):
 def unique(numbers):
     """Given a list of numbers, return a list with duplicates removed.
 
+    >>> unique([0, 1, 2, 3, 4])
+    [0, 1, 2, 3, 4]
+
+    >>> unique([4, 1, 2, 2, 3, 4])
+    [4, 1, 2, 3]
+
+    >>> unique([4, 4, 4, -3, 0, 1])
+    [4, -3, 0, 1]
 
     """
 

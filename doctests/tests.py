dc9623a0048fa4f7ea85718e318d3fe7bb3712bb
Noelle Daley <adriannenoelle@gmail.com>
2015-07-29 19:36:38 -0700

    finished unit test

--- doctests/tests.py
+++ doctests/tests.py
@@ -1,1 +1,13 @@
 import unittest
+from helpers import all_even, unique
+
+class UnitTestCase(unittest.TestCase):
+
+    def test_all_even(self):
+        assert(all_even([0, 1, 2, 3, 4]) == [0, 2, 4])
+
+    def test_unique(self):
+        assert(unique([4, 4, 4, -3, 0, 1]) == [4, -3, 0, 1])
+
+if __name__ == "__main__":
+    unittest.main()
